c735548db7906a97cc5bd8a17f5e938ac1643898
Uday Sharma <udaysharmaniit12345@gmail.com>
2024-08-17 00:01:19 +0530

    Adding 2 String Program to all the student

---
+++ Adding-2-string-programs.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main(){
+	var FirstName string = "Harry"
+	var SecondName string = "Potter"
+	fmt.Println ("Hello", FirstName, SecondName, ",I hope ypu are doing well!")
+
+}
