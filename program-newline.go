2f426ec0a8dbbfa10321b08278590f6eda2468e1
Uday Sharma <udaysharmaniit12345@gmail.com>
2024-08-17 00:12:20 +0530

    Program for New Line

---
+++ program-newline.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	var (
+		user string = "Harry Potter"
+		Age int = 29
+	)
+
+	fmt.Print("Hello"," ", user, " ", "I hope you are doing well! Tell me your age", "\n")
+	fmt.Print("My Age is" , Age)
+}
