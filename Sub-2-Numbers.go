12c6c7bd22dbc3b68c9d5fde9d2f8b21cc7e334d
Uday Sharma <udaysharmaniit12345@gmail.com>
2024-08-17 22:37:40 +0530

    Subtraction of 2 Numbers Program!

---
+++ Sub-2-Numbers.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+
+	var a,b int
+	fmt.Println("Enter the FirstNumber!")
+	fmt.Scanf("%d", &a)
+	fmt.Println("Enter the SecondNumber!")
+	fmt.Scanf("%d", &b)
+
+	var c int
+	c = a - b
+
+	fmt.Println("Result", c)
+}
